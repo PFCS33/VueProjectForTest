ed79fe00ff6dcd7619c7930a7f88d2da3c676968
pfcs33 <aaapfcs@gmail.com>
2023-05-06 17:19:01 +0800

    complete

--- src/App.vue
+++ src/App.vue
@@ -3,12 +3,14 @@
     <header>
       <h1>My Friends</h1>
     </header>
+    <new-friend @add-contact="addContact"></new-friend>
     <ul>
       <friend-contact
         v-for="friend in friends"
         :key="friend.id"
         v-bind="friend"
         @toggle-favorite="toggleFavoriteStatus"
+        @delete="deleteContact"
       ></friend-contact>
     </ul>
   </section>
@@ -21,7 +23,7 @@ export default {
       friends: [
         {
           id: "0",
-          name: "asjodf",
+          name: "aaf",
           phoneNumber: "3214325",
           emailAddress: "832741964hf@324.com",
           isFavorite: true,
@@ -40,6 +42,20 @@ export default {
     toggleFavoriteStatus(id) {
       const identifiedFriend = this.friends.find((friend) => friend.id === id);
       identifiedFriend.isFavorite = !identifiedFriend.isFavorite;
+    },
+    addContact(name, phone, email) {
+      const newFriendContact = {
+        id: new Date().toISOString(),
+        name: name,
+        phoneNumber: phone,
+        emailAddress: email,
+        isFavorite: false,
+      };
+      this.friends.push(newFriendContact);
+    },
+    deleteContact(id) {
+      console.log("!!!");
+      this.friends = this.friends.filter((friend) => friend.id !== id);
     },
   },
 };
@@ -77,7 +93,8 @@ header {
   list-style: none;
 }
 
-#app li {
+#app li,
+#app form {
   box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26);
   margin: 1rem auto;
   border-radius: 10px;
@@ -109,5 +126,18 @@ header {
   background-color: #ec3169;
   border-color: #ec3169;
   box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.26);
+}
+#app input {
+  font: inherit;
+  padding: 0.15rem;
+}
+#app label {
+  font-weight: bold;
+  margin-right: 1rem;
+  width: 7rem;
+  display: inline-block;
+}
+#app form div {
+  margin: 1rem 0;
 }
 </style>
--- src/components/FriendContact.vue
+++ src/components/FriendContact.vue
@@ -9,6 +9,7 @@
       <li><strong>Phone:</strong>{{ phoneNumber }}</li>
       <li><strong>Email:</strong>{{ emailAddress }}</li>
     </ul>
+    <button @click="$emit('delete', id)">delete</button>
   </li>
 </template>
 
@@ -38,10 +39,10 @@ export default {
       default: false,
       // validator: function (value) {
       //   return value === "1" || value === "0";
-      // },
+      // },·
     },
   },
-  emits: ["toggle-favorite"],
+  emits: ["toggle-favorite", "delete"],
   // emits: {
   //   "toggle-favorite": function (id) {
   //     if (id) {
